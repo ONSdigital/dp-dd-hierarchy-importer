0da4f30f6544a0309444014d8ab78890c65512d0
Matt Smith <matthew.smith@thinkinglogic.com>
2017-01-23 15:25:12 +0000

    Changed table names

--- sql/exporter.go
+++ sql/exporter.go
@@ -8,9 +8,9 @@ import (
 )
 
 const (
-	hierarchySQL = "insert into hierarchy (hierarchy_id, hierarchy_name, hierarchy_type) values (%s, %s, %s);\n"
-	areaSQL      = "insert into hierarchy_level_type (id, name, level) values (%s, %s, %d) on conflict do nothing;\n"
-	entrySQL     = "insert into hierarchy_entry (hierarchy_id, entry_code, parent_code, name, level_type, display_order) values (%s, %s, %s, %s, %s, %d);\n"
+	hierarchySQL = "insert into dimension (dimension_id, dimension_name, dimension_type) values (%s, %s, %s);\n"
+	areaSQL      = "insert into dimension_level_type (id, name, level) values (%s, %s, %d) on conflict do nothing;\n"
+	entrySQL     = "insert into dimension_value (dimension_id, value_code, parent_code, name, level_type, display_order) values (%s, %s, %s, %s, %s, %d);\n"
 )
 
 // WriteSQL writes sql insert statements to the given writer to create the given hierarchy in a db.
--- table_structure.sql
+++ table_structure.sql
@@ -1,26 +1,26 @@
-CREATE TABLE hierarchy (
-  hierarchy_id VARCHAR NOT NULL,
-  hierarchy_name VARCHAR,
-  hierarchy_type VARCHAR,
-  PRIMARY KEY( hierarchy_id )
+CREATE TABLE dimension (
+  dimension_id VARCHAR NOT NULL,
+  dimension_name VARCHAR,
+  dimension_type VARCHAR,
+  PRIMARY KEY( dimension_id )
 );
 
-CREATE TABLE hierarchy_level_type (
+CREATE TABLE dimension_level_type (
   id VARCHAR NOT NULL,
   name VARCHAR,
   level INTEGER,
   PRIMARY KEY( id )
 );
 
-CREATE TABLE hierarchy_entry (
-  hierarchy_id VARCHAR NOT NULL,
-  entry_code VARCHAR NOT NULL,
+CREATE TABLE dimension_value (
+  dimension_id VARCHAR NOT NULL,
+  value_code VARCHAR NOT NULL,
   parent_code VARCHAR,
   name VARCHAR,
   level_type VARCHAR,
   display_order INTEGER,
-  PRIMARY KEY( hierarchy_id, entry_code ),
-  FOREIGN KEY (hierarchy_id) REFERENCES hierarchy (hierarchy_id),
-  FOREIGN KEY (level_type) REFERENCES hierarchy_level_type (id),
-  FOREIGN KEY (hierarchy_id, parent_code) REFERENCES hierarchy_entry (hierarchy_id, entry_code)
+  PRIMARY KEY( dimension_id, value_code ),
+  FOREIGN KEY (dimension_id) REFERENCES dimension (dimension_id),
+  FOREIGN KEY (level_type) REFERENCES dimension_level_type (id),
+  FOREIGN KEY (dimension_id, parent_code) REFERENCES dimension_value (dimension_id, value_code)
 );
