203dd9401caa0ae48a27b917a0783dc470b0de9a
Matt Smith <matthew.smith@thinkinglogic.com>
2017-01-24 09:34:38 +0000

    Fixed column names to remove sql keywords

--- table_structure.sql
+++ table_structure.sql
@@ -6,21 +6,21 @@ CREATE TABLE dimension (
 );
 
 CREATE TABLE dimension_level_type (
-  id VARCHAR NOT NULL,
-  name VARCHAR,
-  level INTEGER,
-  PRIMARY KEY( id )
+  type_id VARCHAR NOT NULL,
+  type_name VARCHAR,
+  type_level INTEGER,
+  PRIMARY KEY( type_id )
 );
 
 CREATE TABLE dimension_value (
   dimension_id VARCHAR NOT NULL,
   value_code VARCHAR NOT NULL,
   parent_code VARCHAR,
-  name VARCHAR,
+  value_name VARCHAR,
   level_type VARCHAR,
   display_order INTEGER,
   PRIMARY KEY( dimension_id, value_code ),
   FOREIGN KEY (dimension_id) REFERENCES dimension (dimension_id),
-  FOREIGN KEY (level_type) REFERENCES dimension_level_type (id),
+  FOREIGN KEY (level_type) REFERENCES dimension_level_type (type_id),
   FOREIGN KEY (dimension_id, parent_code) REFERENCES dimension_value (dimension_id, value_code)
 );
